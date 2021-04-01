379a6a89f0f885bcd9c1e79798255f5e64cc301d
Matteo Contrini <matteo@contrini.it>
2021-04-01 22:46:31 +0200

    Add compatibility with text editor drivers

--- extend.php
+++ extend.php
@@ -7,7 +7,7 @@ use Flarum\Extend;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js')
-        ->css(__DIR__ . '/css/forum.css'),
+        ->css(__DIR__ . '/resources/forum.less'),
     (new Extend\Frontend('admin'))
         ->js(__DIR__ . '/js/dist/admin.js'),
     new Extend\Locales(__DIR__ . '/locale'),
